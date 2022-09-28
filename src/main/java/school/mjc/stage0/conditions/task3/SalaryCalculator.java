f86050259decae9bef0f987d6900e37ecabc67e7
mirjambilovj <106926296+mirjambilovj@users.noreply.github.com>
2022-09-28 14:24:19 +0500

    solved

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,5 +2,16 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
+        //Basic Salary <= 10000: taxes - 15%. 10000 < Basic Salary <= 20000: taxes - 18%
+        // Basic Salary > 20000 : taxes - 20% Basic salary < 0 -> "wrong input!"
+        if (salary > 0 && salary <= 10000){
+            System.out.println(salary * 0.85);
+        } else if (salary > 10000 && salary <= 20000) {
+            System.out.println(salary * 0.82);
+        } else if (salary > 20000) {
+            System.out.println(salary * 0.8);
+        } else if (salary < 0) {
+            System.out.println("wrong input!");
+        }
     }
 }
